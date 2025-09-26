c0eee9e14f74ee12af583abb34e595b57c314c67
Zapoye18 <al02953375@tecmilenio.mx>
2025-09-26 12:13:29 -0600

    Prueba para ejecutar el proyecto con dockers

--- RDAT/backend/server.js
+++ RDAT/backend/server.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const connectDB = require("./config/db");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
--- RDAT/frontend/app.js
+++ RDAT/frontend/app.js
@@ -4,7 +4,7 @@ document.getElementById("loginForm").addEventListener("submit", async (e) => {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
-  const res = await fetch("http://localhost:3000/api/auth/login", {
+  const res = await fetch("http://3.141.131.90/5000/api/auth/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ username, password })
