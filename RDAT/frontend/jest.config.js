914dea8a54f46e7a06ee8a00c253087a8b2506e8
R_Mantis <rentalmantis@gmail.com>
2025-09-28 20:28:49 -0600

    Correcciones4

---
+++ RDAT/frontend/jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testMatch: ['**/__tests__/**/*.test.js'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '*.js',
+    '!jest.config.js'
+  ],
+  coverageThreshold: { 
+    global: { 
+      lines: 50, 
+      functions: 50, 
+      statements: 50, 
+      branches: 30 
+    } 
+  }
+};
