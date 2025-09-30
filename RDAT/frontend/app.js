c6f68a5c748fbc67a5f91c8f5e2bf79c33caccdb
R_Mantis <rentalmantis@gmail.com>
2025-09-29 21:11:04 -0600

    version casi terminada 2.3

--- RDAT/frontend/app.js
+++ RDAT/frontend/app.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
       // Enviar datos al backend para validar
       try {
-        const res = await fetch('http://3.141.131.90:5000/api/login', {
+        const res = await fetch('http://localhost:5000/api/login', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ username, password })
